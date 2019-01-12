0ea2bf56fa0a2b1ed030e09e2a22dac8b42db966
Saul Alanis <salanis@localhost.localdomain>
2019-01-12 14:21:02 -0600

    updating README and project script

--- scripts/new_op_project.sh
+++ scripts/new_op_project.sh
@@ -22,22 +22,21 @@
 # 2019.01.12   Saul Alanis           Created script
 # =======================================================================
 #set -x
-# Remove whitespace.
-P=${1// /}
-
-# Set to lowercase.
-PROJECT=`sed -e 's/\(.*\)/\L\1/' <<< "$P"`
-
 # check if string is being passed and whether your GOPATH is set.
-if [[ ! ${PROJECT} ]]; then printf "string required to set your new project name\n"; exit 1;
+if [[ ! ${1} ]]; then printf "string required to set your new project name\n"; exit 1;
 # https://github.com/golang/go/wiki/SettingGOPATH
 elif [[ ! ${GOPATH} ]]; then printf "Please set your GOPATH env\n"; exit 1; fi
 
+# Remove whitespace.
+P=${1// /}
+# Set to lowercase.
+PROJECT=`sed -e 's/\(.*\)/\L\1/' <<< "$P"`
+
 # Set your project path.
-DIR=${GOPATH}/src/github.com/test-project
+DIR=${GOPATH}/src/github.com/test-project/
 # Check if project name exists?
 if [[ ! -d $DIR ]]; then mkdir ${DIR}; exit 0;
-else
+elif [[ -d ${DIR}/${PROJECT} ]]; then
   printf "Project already exists, delete or select a different project name\n"; exit 1; fi
 
 # initiate your operator project.
